28bd8bf70e60a14bef0ff29f703f3bbdc34d4146
Zeeshan Ahmed <zeeshan.ahmed@invozone.com>
2023-05-24 17:28:32 +0500

    Add test cases for message and image encryption.

--- LibSodiumPracticeTests/LibSodiumPracticeTests.swift
+++ LibSodiumPracticeTests/LibSodiumPracticeTests.swift
@@ -2,14 +2,16 @@
 //  LibSodiumPracticeTests.swift
 //  LibSodiumPracticeTests
 //
-//  Created by Arslan Raza on 23/05/2023.
+//  Created by Zeeshan Ahmed on 23/05/2023.
 //
 
 import XCTest
 @testable import LibSodiumPractice
+import Sodium
 
 final class LibSodiumPracticeTests: XCTestCase {
 
+    let sodium = Sodium()
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -31,6 +33,66 @@ final class LibSodiumPracticeTests: XCTestCase {
         self.measure {
             // Put the code you want to measure the time of here.
         }
+    }
+    
+    func testpublickeyCryptography() {
+        
+        let senderKeyPair = sodium.box.keyPair()!
+        let receiverKeyPair = sodium.box.keyPair()!
+        
+        let message = "My Test Message".bytes
+        print("Original Message:\(message.utf8String!)")
+
+        let encryptedMessageFromAliceToBob: Bytes =
+            sodium.box.seal(
+                message: message,
+                recipientPublicKey: receiverKeyPair.publicKey,
+                senderSecretKey: senderKeyPair.secretKey)!
+
+        print("Encrypted Message:\(encryptedMessageFromAliceToBob)")
+
+        let messageVerifiedAndDecryptedByBob =
+            sodium.box.open(
+                nonceAndAuthenticatedCipherText: encryptedMessageFromAliceToBob,
+                senderPublicKey: senderKeyPair.publicKey,
+                recipientSecretKey: receiverKeyPair.secretKey)
+        XCTAssertNotNil(messageVerifiedAndDecryptedByBob)
+        if let decryptedMessage = messageVerifiedAndDecryptedByBob {
+            print(decryptedMessage.utf8String ?? "")
+        }
+    }
+    
+    func testImageWithPublickeyCryptography() {
+        
+        guard let filePath = Bundle(for: type(of: self)).path(forResource: "response", ofType: "png"),
+              let image = UIImage(contentsOfFile: filePath),
+              let data = image.pngData() else {
+            fatalError("Image not available")
+        }
+        
+        let senderKeyPair = sodium.box.keyPair()!
+        let receiverKeyPair = sodium.box.keyPair()!
+        
+        let bytesImage = getArrayOfBytesFromImage(imageData: data as NSData)
+        
+        let encryptedMessageFromAliceToBob: Bytes =
+            sodium.box.seal(
+                message: bytesImage,
+                recipientPublicKey: receiverKeyPair.publicKey,
+                senderSecretKey: senderKeyPair.secretKey)!
+
+        let messageVerifiedAndDecryptedByBob =
+            sodium.box.open(
+                nonceAndAuthenticatedCipherText: encryptedMessageFromAliceToBob,
+                senderPublicKey: senderKeyPair.publicKey,
+                recipientSecretKey: receiverKeyPair.secretKey)
+        
+        let bytesData = Data(messageVerifiedAndDecryptedByBob!)
+        let responseImage = UIImage(data: bytesData)
+        XCTAssertNotNil(responseImage)
+        
+        print("Decrypted Image:\(String(describing: responseImage))")
+
     }
 
 }
